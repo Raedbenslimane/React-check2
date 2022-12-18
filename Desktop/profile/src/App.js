de0e6720da968ece56f4f87299877363b9569a0c
Raed Ben Slimane <bsraed22@gmail.com>
2022-12-18 21:46:46 +0100

    checkpoint 2

---
+++ Desktop/profile/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import img1 from "./Components/photo.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={img1} alt="" height={150} width={150} />
+      <h3> Raed Ben Slimane</h3>
+      <h2> bsraed22@gmail.com</h2>
+    </div>
+  );
+}
+
+export default App;
